61d3b9ef3ba0a70c673dfc5d85de6f879ad16ce8
Daniel Haarhoff <daniel@rknt.de>
2023-11-10 18:41:25 +0000

    Make it possible to load images

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+const updateImagesToDisplay = (preview: HTMLElement, images: HTMLInputElement) => () => {
+  while (preview.firstChild) {
+    preview.removeChild(preview.firstChild);
+  }
+  const curFiles = images.files;
+  console.log(curFiles);
+
+  if (!curFiles) {
+    return;
+  }
+
+  if (curFiles.length === 0) {
+    return;
+  }
+
+  const list = document.createElement('ol');
+  preview.appendChild(list);
+
+  for (const file of curFiles) {
+    const listItem = document.createElement('li');
+    const img = document.createElement('img');
+    img.src = URL.createObjectURL(file);
+
+    listItem.appendChild(img);
+
+    list.appendChild(listItem);
+  }
+};
+
+const images = document.getElementById('images-input') as HTMLInputElement;
+const preview = document.getElementById('images');
+
+if (images && preview) {
+  images.addEventListener('change', updateImagesToDisplay(preview, images));
+  console.log('setup successfull');
+}
