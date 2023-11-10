e63f01d54a609c86ba4bcafd37dcbf37336b3208
Daniel Haarhoff <daniel@rknt.de>
2023-11-10 20:12:44 +0000

    Allow download of all cropped images

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import Cropper from 'cropperjs';
+import { saveAs } from 'file-saver';
+import JSZip from 'jszip';
 
 const imagesInput = document.getElementById('images-input') as HTMLInputElement;
 const croppingDiv = document.getElementById('images');
+const downloadButton = document.getElementById('download');
 const cropWidthInput = <HTMLInputElement>document.getElementById('crop-width');
 const cropHeightInput = <HTMLInputElement>document.getElementById('crop-height');
 
-let croppers: Array<Cropper> = [];
+let croppers: Array<{ cropper: Cropper; imgName: string }> = [];
 
 const updateImagesToDisplay = (preview: HTMLElement, images: HTMLInputElement) => () => {
   while (preview.firstChild) {
@@ -48,7 +51,7 @@ const updateImagesToDisplay = (preview: HTMLElement, images: HTMLInputElement) =
     slider.value = '0';
     slider.addEventListener('input', () => cropper.setData({ rotate: parseInt(slider.value) }));
 
-    croppers.push(cropper);
+    croppers.push({ cropper, imgName: file.name });
     listItem.appendChild(img);
     listItem.appendChild(slider);
     list.appendChild(listItem);
@@ -58,18 +61,39 @@ const updateImagesToDisplay = (preview: HTMLElement, images: HTMLInputElement) =
 const updateCropSize = () => {
   console.log(cropHeightInput.value);
   for (const cropper of croppers) {
-    cropper.setData({
+    cropper.cropper.setData({
       width: parseInt(cropWidthInput.value),
       height: parseInt(cropHeightInput.value),
     });
   }
 };
 
-if (imagesInput && croppingDiv) {
+const downloadAll = () => {
+  const zip = new JSZip();
+  for (const cropper of croppers) {
+    cropper.cropper.getCroppedCanvas().toBlob((blob) => {
+      if (!blob) {
+        return;
+      }
+      zip.file(`${cropper.imgName}.png`, blob);
+      zip
+        .generateAsync({
+          type: 'blob',
+        })
+        .then(function (content) {
+          saveAs(content, 'cropped.zip');
+        });
+    });
+  }
+};
+
+if (imagesInput && croppingDiv && downloadButton) {
   cropWidthInput.addEventListener('change', updateCropSize);
   cropHeightInput.addEventListener('change', updateCropSize);
 
   imagesInput.value = '';
   imagesInput.addEventListener('change', updateImagesToDisplay(croppingDiv, imagesInput));
   console.log('setup successfull');
+
+  downloadButton.addEventListener('click', downloadAll);
 }
